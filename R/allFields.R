1f2ef0ec1218074f8b3b52152d4da6cf035844eb
Carson Sievert <cpsievert1@gmail.com>
2012-08-08 23:21:15 -0500

    whoops. transfer from dropbox

---
+++ R/allFields.R
@@ -0,0 +1,9 @@
+#' All the field names for Tables of interest in pitchRx package
+#' 
+#' A list of sets of field names (used to reduce parsing time within \code{scrapePitchFX}).
+#' 
+#' @docType data
+#' @name allFields
+#' @author Carson Sievert
+#'
+NULL
