b769885a456f7550bf7d9f3d86704953ba4d1ddf
Carson Sievert <cpsievert1@gmail.com>
2013-01-15 10:44:12 -0600

    avoid error in getFacets when no layer exists

--- R/getFacets.R
+++ R/getFacets.R
@@ -6,6 +6,7 @@
 #' @result character vector with variable names (if faceting is present). Otherwise, \code{NULL}
 #' 
 getFacets <- function(layers){
+  if (length(layers) == 0) return(NULL)
   rows <- layers$rows
   cols <- layers$cols
   if (length(rows) > 0) {
