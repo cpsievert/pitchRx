86bceb1e4170e44bdeaef0dc9fc64b387d258447
Carson Sievert <cpsievert1@gmail.com>
2015-10-08 19:38:32 +1100

    more fixes for CRAN

--- R/help.R
+++ R/help.R
@@ -1,8 +1,11 @@
 #' PITCHf/x package
-#' 
+#'
 #' @docType package
 #' @name pitchRx
 #' @author Carson Sievert
 #' @seealso \url{http://cpsievert.github.com/pitchRx}
+#' @importFrom grDevices rgb terrain.colors
+#' @importFrom stats complete.cases predict reorder setNames
+#' @importFrom utils data packageVersion write.csv
 #'
 NULL
