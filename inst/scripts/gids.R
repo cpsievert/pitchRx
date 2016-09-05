88c0f1c1823fe669aa14c8fe7b212a98817a52f8
Carson Sievert <cpsievert1@gmail.com>
2016-09-05 12:16:43 -0500

    update gids

--- inst/scripts/gids.R
+++ inst/scripts/gids.R
@@ -5,4 +5,4 @@ last.game <- sort(gids)[length(gids)]
 last.date <- as.Date(substr(last.game, 5, 14), format = "%Y_%m_%d")
 new.gids <- pitchRx:::updateGids(last.date, last.date + 371)
 gids <- unique(c(gids, new.gids))
-save(gids, file = "~/Desktop/github/local/pitchRx/data/gids.rda")
+devtools::use_data(gids, overwrite = TRUE)
