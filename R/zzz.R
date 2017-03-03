5aab8207cdd4a7bd544f4d3cf7a5d2111255e2e2
Keberwein <kristian.eberwein@yahoo.com>
2017-03-03 16:31:16 -0500

    innings extract

--- R/zzz.R
+++ R/zzz.R
@@ -1,26 +1,31 @@
-z=scrape(game.ids="gid_2010_06_01_lhvaaa_tolaaa_1")
+z=scrape(game.ids=c("gid_2010_06_01_lhvaaa_tolaaa_1", "gid_2010_06_02_albaaa_nasaaa_1"))
 
 
 zz=scrape(game.ids="gid_2011_04_04_minmlb_nyamlb_1")
 
 
-# Check to see if any game.ids are minor league
-if(substr(game.ids, nchar(game.ids)-4, nchar(game.ids)-2)!="mlb") {
+# Check to see if any game.ids are minor league.
+if(any(substr(game.ids, nchar(game.ids)-4, nchar(game.ids)-2)!="mlb")) {
   print("boo")
 }
 
 
+gamzList <- list()
+for (i in length(gameDir)) {
+  inningz <- readLines(paste0(gameDir[i], "/inning"))
+  inningzList[[i]] <- inningz
+}
 
 
-inningz <- readLines("http://gd2.mlb.com/components/game/aaa/year_2010/month_06/day_01/gid_2010_06_01_lhvaaa_tolaaa_1/inning")
 
-inning_num=1:9
+# Make place holder for game innings.
+inningzList <- list()
+# Assume there were a ton of innings.
+inning_num=1:25
 for (i in inning_num) {
-
-  if (any(grep(paste0("inning_", inning_num, ".xml", sep="", collapse="|"), inningz))) {
-  valz <- paste0("inning_", inning_num, ".xml", sep=",", collapse="|")
-  print("boo")
-
+  if (isTRUE(any(grepl(paste0("inning_", i, ".xml", sep="", collapse="|"), inningzList)))) {
+  valz <- paste0("inning_", i, ".xml", sep=",", collapse="|")
+  inningzList[[i]] <- valz
   }
 }
-
+df <- do.call(rbind, inningzList)
