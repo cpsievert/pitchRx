7ac8891fb0e5f6ddb0dc5985aaffa11f97f74372
James Ingold <jamesp.ingold@gmail.com>
2020-03-01 15:18:15 -0600

    replacing root gameday day directory with master scoreboard
    updating gids.rda with 2020 season

--- inst/scripts/gids.R
+++ inst/scripts/gids.R
@@ -1,19 +1,18 @@
 # Grab new game IDs
 library(pitchRx)
 library(rvest)
+library(jsonlite)
 data(gids)
 last.game <- sort(gids)[length(gids)]
 last.date <- as.Date(substr(last.game, 5, 14), format = "%Y_%m_%d")
 day_urls <- pitchRx:::makeUrls(last.date, last.date + 371, gids="")
-day_list <- lapply(day_urls, xml2::read_html)
+scoreboards <- paste0(day_urls, "/master_scoreboard.json")
+day_list <- lapply(scoreboards, fromJSON)
+
 new_gids <- lapply(day_list, function(day) {
-  day %>%
-    html_nodes("a") %>%
-    html_text() %>%
-    stringr::str_trim() %>%
-    stringr::str_remove("/$") %>%
-    grep("^gid_", ., value = TRUE)
+  payload <- day$data$games$game$gameday
 })
 new_gids <- unlist(new_gids)
+new_gids <- paste0('gid_', new_gids)
 gids <- unique(c(gids, new_gids))
 devtools::use_data(gids, overwrite = TRUE)
